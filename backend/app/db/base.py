339267540483b05afdc5e40917143f24c33cb3c0
schechtereddie <schechtereddie@users.gmail.com>
2025-10-31 01:11:33 -0700

    backend: update Base to use DeclarativeBase class

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,6 +1,4 @@
-"""
-Database Base Class
-"""
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
